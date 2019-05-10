44ff370d2aa1040d7df312d4f173e964e52744bc
d-shimizu <shimizu.daisuke.sd@gmail.com>
2019-05-10 23:20:17 +0900

    Add 'Creating a slice with make' making-slice.go

--- 02_Basics/more_types-structs-slices-and-maps/making-slice.go
+++ 02_Basics/more_types-structs-slices-and-maps/making-slice.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// スライスは、組み込みのmake関数を使用して作成することができる。
+	// これは動的サイズの配列を作成する方法
+	// make関数はゼロ化された配列を割り当て、その配列を指すスライスを返す。
 	a := make([]int, 5)
 	printSlice("a", a)
 
